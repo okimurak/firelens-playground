8b11aa532d54cd24276204b8f2cc0464f53c66fc
koichiro-okimura <koichiro-okimura@dac.co.jp>
2020-08-20 10:52:50 +0900

    feat: add fluentd ecr repository

--- terraform/main.tf
+++ terraform/main.tf
@@ -144,3 +144,31 @@ resource "aws_ecr_lifecycle_policy" "example" {
   }
 EOF
 }
+
+resource "aws_ecr_repository" "fluentd" {
+  name = "fluentd"
+}
+
+resource "aws_ecr_lifecycle_policy" "fluentd" {
+  repository = aws_ecr_repository.fluentd.name
+
+  policy = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 30 release tagged images",
+        "selection": {
+        "tagStatus": "tagged",
+        "tagPrefixList": ["release"],
+        "countType": "imageCountMoreThan",
+        "countNumber": 30
+        },
+        "action": {
+        "type": "expire"
+        }
+      }
+    ]
+  }
+EOF
+}
